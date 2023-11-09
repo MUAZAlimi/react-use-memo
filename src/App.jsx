e43866e42d5afc5c085a98acec33158218e87dc2
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-09 10:27:25 +0100

    use-memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState, useCallback, useMemo } from "react";
 
+
 const getArray = () => {
-  for(let i = 0; i < 1000000000; i++) {
-    
-  }
+  for(let i = 0; i < 1000000000; i++) {}
   return ['Muhammed', 'Fine Boy']
 }
 
 const App = () => {
   const [userInput, setUserInput] = useState("");
-  
+  const [randomInput, setRandomInput] = useState("")
+    
+  const fib = useCallback((n) => {
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2)
+  }, [])
+
   return (
     <main className="App">
       <input
